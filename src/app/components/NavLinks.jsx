713bd9fca488798acdab9279b691ddeb25afda89
Roshaan <roshaanahmad91@gmail.com>
2026-06-11 04:52:13 +0500

    fix: add click handler to navigation links for improved user interaction

--- src/app/components/NavLinks.jsx
+++ src/app/components/NavLinks.jsx
@@ -6,19 +6,19 @@ const NavLinks = ({handleDropDown}) => {
   return (
     <div>
       <nav className={styles.navLinks} onClick={handleDropDown}>
-        <Link className={styles.links} href="/">
+        <Link className={styles.links} onClick={handleDropDown} href="/">
         <i className="fa-solid fa-house"></i> HOME
         </Link>
-        <Link className={styles.links} href="/contact">
+        <Link className={styles.links} onClick={handleDropDown} href="/contact">
         <i className="fa-solid fa-phone"></i> CONTACTS
         </Link>
-        <Link className={styles.links} href="/pastpapers">
+        <Link className={styles.links} onClick={handleDropDown} href="/pastpapers">
         <i className="fa-solid fa-info-circle"></i> Past-Papers
         </Link>
-        <Link className={styles.links} href="/blogs">
+        <Link className={styles.links} onClick={handleDropDown} href="/blogs">
         <i className="fa-solid fa-blog"></i> BLOGS
         </Link>
-        <Link className={styles.links} href="/semester">
+        <Link className={styles.links} onClick={handleDropDown} href="/semester">
         <i className="fa-solid fa-school"></i> Semesters
         </Link>
       </nav>
